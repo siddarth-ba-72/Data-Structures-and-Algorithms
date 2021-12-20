eac99b3cb8015253f6824ace15ec7d10c5855c80
Siddarth72 <72@siddarth.com>
2021-12-20 19:42:52 +0530

    Data Structures And Algorithms

--- tree/balancedBST.c
+++ tree/balancedBST.c
@@ -1,117 +1,171 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZEOF(arr) sizeof(arr) / sizeof(*arr)
+#include <math.h>
 
-struct Tree
+struct node
 {
 	int data;
-	struct Tree *left;
-	struct Tree *right;
+	struct node *left;
+	struct node *right;
 };
 
-typedef struct Tree *tree;
+typedef struct node *Node;
 
-// ? Function to create a new tree node
-tree newNode(int data)
+// ? Creating a new Tree Node
+Node newNode(int value)
 {
-	tree root = (tree)malloc(sizeof(struct Tree));
-	root->data = data;
-	root->left = NULL;
-	root->right = NULL;
-	return root;
-}
-
-// ? Insert a key in a tree
-tree insert(tree root, int key)
-{
-	tree temp = (tree)malloc(sizeof(struct Tree));
-	printf("Enter the key: ");
-	scanf("%d", key);
-	temp->data = key;
+	Node temp = (Node)malloc(sizeof(struct node));
+	temp->data = value;
 	temp->left = temp->right = NULL;
+	return temp;
+}
+
+// ? Inserting a new Node into the tree
+Node insert(Node root, int value)
+{
 	if (root == NULL)
-		return temp;
-	if (key < root->data)
-		root->left = insert(root->left, key);
-	else
-		root->right = insert(root->right, key);
+		return newNode(value);
+	if (value < root->data)
+		root->left = insert(root->left, value);
+	else if (value > root->data)
+		root->right = insert(root->right, value);
 	return root;
 }
 
-// ? Sorting an array of integers
-void sort(int arr[], int n)
+// ? Inorder Traversal of Tree
+void inorderTraversal(Node root)
 {
-	int i, j, temp;
-	for (i = 0; i < n - 1; i++)
+	if (root != NULL)
 	{
-		for (j = 0; j < n - i - 1; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
+		inorderTraversal(root->left);
+		printf("%d ", root->data);
+		inorderTraversal(root->right);
 	}
 }
 
-// ? Convert balanced BST from given array of integers
-tree arrToBst(int arr[], int low, int high, int n)
-{
-	sort(arr, n);
-	if (low > high)
-		return NULL;
-	int mid = (low + high) / 2;
-	tree root = newNode(arr[mid]);
-	root->left = arrToBst(arr, low, mid - 1, n);
-	root->right = arrToBst(arr, mid + 1, high, n);
-	return root;
-}
-
-// ? Inorder traversal of a tree
-void inorderTraversal(tree root)
-{
-	if (root == NULL)
-		return;
-	inorderTraversal(root->left);
-	printf("%d ", root->data);
-	inorderTraversal(root->right);
-}
-
-// ? Preorder traversal of a tree
-void preorderTraversal(tree root)
+// ? Preorder traversal of a node
+void preorderTraversal(Node root)
 {
 	if (root == NULL)
 		return;
 	printf("%d ", root->data);
 	preorderTraversal(root->left);
 	preorderTraversal(root->right);
+	printf("\n");
 }
 
-// ? Postorder traversal of a tree
-void postorderTraversal(tree root)
+// ? Postorder traversal of a node
+void postorderTraversal(Node root)
 {
 	if (root == NULL)
 		return;
 	postorderTraversal(root->left);
 	postorderTraversal(root->right);
 	printf("%d ", root->data);
+	printf("\n");
 }
+
+// ? Maximum value in a tree
+int maxNum(Node root)
+{
+	if (root == NULL)
+		return -1;
+	Node curr = root;
+	while (curr->right != NULL)
+		curr = curr->right;
+	return curr->data;
+}
+
+// ? Minimum value in a tree
+int minNum(Node root)
+{
+	if (root == NULL)
+		return -1;
+	Node curr = root;
+	while (curr->left != NULL)
+		curr = curr->left;
+	return curr->data;
+}
+
+// ? Finding the height of a tree
+int heightBST(Node root)
+{
+	if (root == NULL)
+		return -1;
+	int lDeapth = heightBST(root->left);
+	int rDeapth = heightBST(root->right);
+	return (lDeapth > rDeapth) ? lDeapth + 1 : rDeapth + 1;
+}
+
+// ? Number of nodes in the tree
+int numOfNodes(Node root)
+{
+	if (root == NULL)
+		return 0;
+	return 1 + numOfNodes(root->left) + numOfNodes(root->right);
+}
+
+// ? Number of leaf nodes in a tree
+int numOfLeafNodes(Node root)
+{
+	if (root == NULL)
+		return 0;
+	if (root->left == NULL && root->right == NULL)
+		return 1;
+	return numOfLeafNodes(root->left) + numOfLeafNodes(root->right);
+}
+
+// ? Sorting an array of integers
+// void sort(int arr[], int n)
+// {
+// 	int i, j, temp;
+// 	for (i = 0; i < n - 1; i++)
+// 	{
+// 		for (j = 0; j < n - i - 1; j++)
+// 		{
+// 			if (arr[j] > arr[j + 1])
+// 			{
+// 				temp = arr[j];
+// 				arr[j] = arr[j + 1];
+// 				arr[j + 1] = temp;
+// 			}
+// 		}
+// 	}
+// }
+
+// ? Convert to balanced BST from given array of integers
+// Node arrToBst(int arr[], int low, int high, int n)
+// {
+// 	sort(arr, n);
+// 	if (low > high)
+// 		return NULL;
+// 	int mid = (low + high) / 2;
+// 	Node root = newNode(arr[mid]);
+// 	root->left = arrToBst(arr, low, mid - 1, n);
+// 	root->right = arrToBst(arr, mid + 1, high, n);
+// 	return root;
+// }
 
 int main()
 {
-	int arr[] = {15, 10, 20, 8, 12, 16, 25};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	tree root = arrToBst(arr, 0, n - 1, n);
-	printf("Inorder: ");
+	Node root = NULL;
+	root = insert(root, 50);
+	insert(root, 30);
+	insert(root, 20);
+	insert(root, 40);
+	insert(root, 70);
+	insert(root, 60);
+	insert(root, 80);
 	inorderTraversal(root);
 	printf("\n");
-	printf("Preorder: ");
-	preorderTraversal(root);
-	printf("\n");
-	printf("Postorder: ");
-	postorderTraversal(root);
-	printf("\n");
+	printf("Max Value = %d\n", maxNum(root));
+	printf("Min Value = %d\n", minNum(root));
+	printf("Height of Tree = %d\n", heightBST(root));
+	printf("Number of Nodes = %d\n", numOfNodes(root));
+	printf("Number of Leaf Nodes = %d\n", numOfLeafNodes(root));
+
 	return 0;
 }
+/*
+? Q6. Function to delete a node in a tree
+*/
